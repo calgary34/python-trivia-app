0398272efa48c3415318a15f935527f47422e9ea
calgary34 <51468959+calgary34@users.noreply.github.com>
2022-01-24 21:53:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,13 @@ while True:
         selected_answer = int(input("Enter your answer: "))
     except ValueError:
         print("Error: invalid number")
-
-    if answers[selected_answer - 1] == current_question['correct']:
-        print("You answered correctly!")
-    else:
-        print("Your answer wasn't correct. (womp womp)")
+        break
+    
+    try:
+        if answers[selected_answer - 1] == current_question['correct']:
+            print("You answered correctly!")
+        else:
+            print("Your answer wasn't correct. (womp womp)")
+    except IndexError:
+        print("Error: invalid answer")
+        break
